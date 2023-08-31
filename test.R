eac89249dd632123fca866fb11aed52665fa7d5a
Jill MacKay <jmackay4@ed.ac.uk>
2023-08-31 17:07:16 +0100

    fleshed out bones

--- test.R
+++ test.R
@@ -43,3 +43,61 @@ teaching <- tibble("Public Engagement" = c("Open Educational Resources (MOOCs)")
                                                      "Doctorate in Veterinary Medicine (Resident)**"),
                    "Postgraduate Research" = c("Masters by Research",
                                                "Doctor of Philosophy (PhD)"))
+
+
+teaching <- tibble(Programme = c("Open Educational Resources (MOOCs)",
+                                 "Bachelors of Veterinary Medicine and Surgery",
+                                 "Bachelors of Science (Agriculture)",
+                                 "Bachelors of Science (Veterinary Science)*",
+                                 "Applied Animal Behaviour and Animal Welfare (On Campus)",
+                                 "Animal Biosciences (On Campus)",
+                                 "Planetary Health (Blended)",
+                                 "Sustainable Lands and Cities (Blended)",
+                                 "Advanced Clinical Practice (Online)",
+                                 "Advanced Veterinary Practice (Online)",
+                                 "Applied Conservation Genetics with Wildlife Forensics (Online)",
+                                 "Applied Poultry Science (Online)",
+                                 "Clinical Animal Behaviour (Online)",
+                                 "Conservation Medicine (Online)",
+                                 "Equine Science (Online)",
+                                 "Food Safety (Online)",
+                                 "Global Food Security and Nutrition (Online)",
+                                 "International Animal Welfare, Ethics and Law (Online)",
+                                 "One Health (Online)",
+                                 "Veterinary Anaesthesia and Analgesia (Online)",
+                                 "Doctorate in Veterinary Medicine (Resident)**",
+                                 "Masters by Research",
+                                 "Doctor of Philosophy (PhD)"),
+                   Grouping = c("Public Engagement",
+                                "Undergraduate",
+                                "Undergraduate",
+                                "Undergraduate",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Taught",
+                                "Postgraduate Research",
+                                "Postgraduate Research"))
+
+
+
+
+teaching %>% 
+  pivot_wider(names_from = "Grouping",values_from = "Programme") %>% 
+  mutate(`Postgraduate Taught` = str_replace(`Postgraduate Taught`, ",", ", \\n"))
+
+
+
